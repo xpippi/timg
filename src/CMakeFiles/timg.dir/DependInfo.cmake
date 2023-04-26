298ca109535abe7ef22ec236eb92a6ca7983be75
root <root@penguin>
2023-04-26 05:24:46 +0200

    ..

---
+++ src/CMakeFiles/timg.dir/DependInfo.cmake
@@ -0,0 +1,54 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/chromeos/MyFiles/timg-main/src/buffered-write-sequencer.cc" "/mnt/chromeos/MyFiles/timg-main/src/CMakeFiles/timg.dir/buffered-write-sequencer.cc.o"
+  "/mnt/chromeos/MyFiles/timg-main/src/framebuffer.cc" "/mnt/chromeos/MyFiles/timg-main/src/CMakeFiles/timg.dir/framebuffer.cc.o"
+  "/mnt/chromeos/MyFiles/timg-main/src/image-display.cc" "/mnt/chromeos/MyFiles/timg-main/src/CMakeFiles/timg.dir/image-display.cc.o"
+  "/mnt/chromeos/MyFiles/timg-main/src/image-source.cc" "/mnt/chromeos/MyFiles/timg-main/src/CMakeFiles/timg.dir/image-source.cc.o"
+  "/mnt/chromeos/MyFiles/timg-main/src/iterm2-canvas.cc" "/mnt/chromeos/MyFiles/timg-main/src/CMakeFiles/timg.dir/iterm2-canvas.cc.o"
+  "/mnt/chromeos/MyFiles/timg-main/src/jpeg-source.cc" "/mnt/chromeos/MyFiles/timg-main/src/CMakeFiles/timg.dir/jpeg-source.cc.o"
+  "/mnt/chromeos/MyFiles/timg-main/src/kitty-canvas.cc" "/mnt/chromeos/MyFiles/timg-main/src/CMakeFiles/timg.dir/kitty-canvas.cc.o"
+  "/mnt/chromeos/MyFiles/timg-main/src/openslide-source.cc" "/mnt/chromeos/MyFiles/timg-main/src/CMakeFiles/timg.dir/openslide-source.cc.o"
+  "/mnt/chromeos/MyFiles/timg-main/src/qoi-image-source.cc" "/mnt/chromeos/MyFiles/timg-main/src/CMakeFiles/timg.dir/qoi-image-source.cc.o"
+  "/mnt/chromeos/MyFiles/timg-main/src/renderer.cc" "/mnt/chromeos/MyFiles/timg-main/src/CMakeFiles/timg.dir/renderer.cc.o"
+  "/mnt/chromeos/MyFiles/timg-main/src/stb-image-source.cc" "/mnt/chromeos/MyFiles/timg-main/src/CMakeFiles/timg.dir/stb-image-source.cc.o"
+  "/mnt/chromeos/MyFiles/timg-main/src/term-query.cc" "/mnt/chromeos/MyFiles/timg-main/src/CMakeFiles/timg.dir/term-query.cc.o"
+  "/mnt/chromeos/MyFiles/timg-main/src/terminal-canvas.cc" "/mnt/chromeos/MyFiles/timg-main/src/CMakeFiles/timg.dir/terminal-canvas.cc.o"
+  "/mnt/chromeos/MyFiles/timg-main/src/termutils.cc" "/mnt/chromeos/MyFiles/timg-main/src/CMakeFiles/timg.dir/termutils.cc.o"
+  "/mnt/chromeos/MyFiles/timg-main/src/timg-png.cc" "/mnt/chromeos/MyFiles/timg-main/src/CMakeFiles/timg.dir/timg-png.cc.o"
+  "/mnt/chromeos/MyFiles/timg-main/src/timg.cc" "/mnt/chromeos/MyFiles/timg-main/src/CMakeFiles/timg.dir/timg.cc.o"
+  "/mnt/chromeos/MyFiles/timg-main/src/unicode-block-canvas.cc" "/mnt/chromeos/MyFiles/timg-main/src/CMakeFiles/timg.dir/unicode-block-canvas.cc.o"
+  "/mnt/chromeos/MyFiles/timg-main/src/video-display.cc" "/mnt/chromeos/MyFiles/timg-main/src/CMakeFiles/timg.dir/video-display.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "HAVE_AVDEVICE"
+  "WITH_TIMG_GRPAPHICSMAGICK"
+  "WITH_TIMG_JPEG"
+  "WITH_TIMG_OPENSLIDE_SUPPORT"
+  "WITH_TIMG_QOI"
+  "WITH_TIMG_STB"
+  "WITH_TIMG_TERMINAL_QUERY"
+  "WITH_TIMG_VIDEO"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "src"
+  "third_party"
+  "third_party/qoi"
+  "/usr/include/GraphicsMagick"
+  "/usr/include/openslide"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/CMakeFiles/timg.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/timg.dir/buffered-write-sequencer.cc.o"
+  "CMakeFiles/timg.dir/framebuffer.cc.o"
+  "CMakeFiles/timg.dir/image-display.cc.o"
+  "CMakeFiles/timg.dir/image-source.cc.o"
+  "CMakeFiles/timg.dir/iterm2-canvas.cc.o"
+  "CMakeFiles/timg.dir/jpeg-source.cc.o"
+  "CMakeFiles/timg.dir/kitty-canvas.cc.o"
+  "CMakeFiles/timg.dir/openslide-source.cc.o"
+  "CMakeFiles/timg.dir/qoi-image-source.cc.o"
+  "CMakeFiles/timg.dir/renderer.cc.o"
+  "CMakeFiles/timg.dir/stb-image-source.cc.o"
+  "CMakeFiles/timg.dir/term-query.cc.o"
+  "CMakeFiles/timg.dir/terminal-canvas.cc.o"
+  "CMakeFiles/timg.dir/termutils.cc.o"
+  "CMakeFiles/timg.dir/timg-png.cc.o"
+  "CMakeFiles/timg.dir/timg.cc.o"
+  "CMakeFiles/timg.dir/unicode-block-canvas.cc.o"
+  "CMakeFiles/timg.dir/video-display.cc.o"
+  "timg"
+  "timg.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/timg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
